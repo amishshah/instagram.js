321276728b3d4fb44775412b875e490dc4974dda
Amish Shah <amishshah.2k@gmail.com>
2017-04-14 12:27:45 +0100

    Add HTTP verbs as methods

--- src/Client.js
+++ src/Client.js
@@ -10,25 +10,30 @@ class InstagramClient {
     return new Promise((resolve, reject) => {
       parameters.access_token = this.token;
       parameters = Object.entries(parameters).map(pair => `${pair[0]}=${encodeURIComponent(pair[1])}`).join('&');
-      request[verb](`${endpoint}?${parameters}`).end((e, r) => e ? reject(e) : resolve(r.body.data));
+      request[verb](`${endpoint}?${parameters}`).end((e, r) => e ? reject(e) : resolve(r.body));
     });
   }
 
+  get(endpoint, parameters) { return this._api('get', endpoint, parameters); }
+  post(endpoint, parameters) { return this._api('post', endpoint, parameters); }
+  put(endpoint, parameters) { return this._api('put', endpoint, parameters); }
+  del(endpoint, parameters) { return this._api('del', endpoint, parameters); }
+
   // USERS endpoints
   getUser(id) {
-    return this._api('get', Endpoints.Users.User(id));
+    return this.get(Endpoints.Users.User(id));
   }
 
   getUserMediaRecent(id, parameters) {
-    return this._api('get', Endpoints.Users.Media.Recent(id), parameters);
+    return this.get(Endpoints.Users.Media.Recent(id), parameters);
   }
 
   getSelfMediaLiked(parameters) {
-    return this._api('get', Endpoints.Users.Media.Liked('self'), parameters);
+    return this.get(Endpoints.Users.Media.Liked('self'), parameters);
   }
 
   searchUsers(parameters) {
-    return this._api('get', Endpoints.Users.Search, parameters);
+    return this.get(Endpoints.Users.Search, parameters);
   }
 }
 
